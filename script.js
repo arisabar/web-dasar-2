38ed0896273f8ee938cc6536f0b82f32ad978337
arisabar <arisabar_subakti@yahoo.co.id>
2022-06-23 17:47:02 +0800

    save 1

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-let index = 1;
-slideShow(index);
-
-function slideShow(n) {
-  const article = document.querySelectorAll("article");
-  const click = document.querySelectorAll(".click");
-
-  const nav = Array.from(click);
-  if (n > article.length) {
-    index = 1;
+function openPage(pageName) {
+  const tabContent = document.getElementsByClassName("content");
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
   }
-  if (n < 1) {
-    index = article.length;
-  }
-
-  for (let i = 0; i < nav.length; i++) {
-    nav[i].className = nav[i].className.replace(" active", "");
-  }
-
-  article[index - 1].style.display = "block";
-  nav[index - 1].className += " active";
-  console.log();
+  document.getElementById(pageName).style.display = "block";
 }
