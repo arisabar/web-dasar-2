c9e32d561c6b24a92e85689c4066ead079ef42f8
arisabar <arisabar_subakti@yahoo.co.id>
2022-06-24 16:32:37 +0800

    penambahan dark mode

--- script.js
+++ script.js
@@ -3,5 +3,34 @@ function openPage(pageName) {
   for (let i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = "none";
   }
-  document.getElementById(pageName).style.display = "block";
+  console.log(tabContent);
+  let pages = (document.getElementById(pageName).style.display = "block");
+  return pages;
 }
+
+const mode = document.getElementById("dark");
+
+if (localStorage.getItem("theme") == null) {
+  localStorage.setItem("theme", "light");
+}
+
+let localData = localStorage.getItem("theme");
+
+if (localData == "light") {
+  mode.innerText = "Dark Mode";
+  document.body.classList.remove("dark-theme");
+} else if (localData == "dark") {
+  mode.innerText = "Light Mode";
+  document.body.classList.add("dark-theme");
+}
+
+mode.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    mode.innerText = "Light Mode";
+    localStorage.setItem("theme", "dark");
+  } else {
+    mode.innerText = "Dark Mode";
+    localStorage.setItem("theme", "light");
+  }
+});
